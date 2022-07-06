2ef6df09d49f07a2823b119c8cb50c0f78b5e64d
Lienid <0xLienid@protonmail.com>
2022-07-06 11:56:19 -0400

    Add strategy encoding for Uniswap

--- src/incurDebt.ts
+++ src/incurDebt.ts
@@ -1,21 +1,34 @@
-import { Contract } from "ethers";
+import { Contract, providers } from "ethers";
+import { IncurDebtABI } from "./abi/abis";
+import { IncurDebtAddress, ohm, Strategies } from "./constants";
+import { Context } from "./context";
+import { Uniswap } from "./strategies/Uniswap";
 
-export const IncurDebtAddress = "0x0000000000000000000000000000000000000000";
-
-export const IncurDebtABI = [
-  "function deposit(uint256) external",
-  "function borrow(uint256) external",
-  "function createLP(uint256, address, bytes) external returns (uint256)",
-  "function removeLP(uint256, address, address, bytes) external returns (uint256)",
-  "function withdrawLP(uint256, address) external",
-  "function withdraw(uint256) external",
-  "function repayDebtWithCollateral() public",
-  "function repayDebtWithCollateralAndWithdrawTheRest() external",
-  "function repayDebtWithOHM(uint256 _ohmAmount) external",
-  "function getAvailableToBorrow() public view returns (uint256)",
-];
+type JsonRpcProvider = providers.JsonRpcProvider;
 
 export class IncurDebt {
-  static abi = IncurDebtABI;
-  //  protected contract: Contract = new Contract(IncurDebtAddress, IncurDebtABI, )
+    private abi = IncurDebtABI;
+
+    private strategies: { [key: string]: string } = Strategies;
+
+    private context = new Context("https://mainnet.infura.io/v3/9a9cc37c69df4dcab372cc09acc4598c");
+
+    private provider: JsonRpcProvider;
+    
+    protected incurDebtContract: Contract;
+
+    constructor() {
+        this.provider = this.context.getProvider();
+
+        this.incurDebtContract = new Contract(IncurDebtAddress, this.abi, this.provider);
+    }
+
+    async encodeBorrowParameters(strategy: string, lpAddress: string, slippage: number = 0.05, ohmAmount: string) {
+        if (!this.strategies[strategy]) throw new Error("The only available strategies are Curve, Uniswap, Sushiswap, or Balancer.");
+
+        if (strategy == "uniswap" || strategy == "sushiswap") {
+            const UniStrategy = new Uniswap(lpAddress, slippage, ohmAmount, this.provider);
+            return UniStrategy.getEncodedParams();
+        }
+    }
 }
---
+++ src/strategies/Uniswap.ts
@@ -0,0 +1,75 @@
+import { BigNumber, Contract, providers } from "ethers";
+import { defaultAbiCoder as  abiCoder } from "ethers/lib/utils";
+import { UniV2PoolABI } from "../abi/abis";
+import { ohm } from "../constants";
+
+type JsonRpcProvider = providers.JsonRpcProvider;
+
+export class Uniswap {
+    private abi = UniV2PoolABI;
+
+    private liquidityPool: Contract;
+
+    private acceptableSlippage: number;
+
+    private ohmToBorrow: string;
+
+    constructor(lpAddress: string, slippage: number = 0.05, ohmAmount: string, provider: JsonRpcProvider) {
+        this.liquidityPool = new Contract(lpAddress, this.abi, provider);
+
+        this.acceptableSlippage = 1 - slippage;
+
+        this.ohmToBorrow = ohmAmount;
+    }
+
+    async getTokenA(): Promise<string> {
+        if (!this.liquidityPool) throw new Error("Liquidity pool not initialized");
+        return this.liquidityPool.token0();
+    }
+
+    async getTokenB(): Promise<string> {
+        if (!this.liquidityPool) throw new Error("Liquidity pool not initialized");
+        return this.liquidityPool.token1();
+    }
+
+    async getReserveRatio(): Promise<string> {
+        if (!this.liquidityPool) throw new Error("Liquidity pool not initialized");
+        
+        const {reserves0, reserves1, lastTimestamp} = await this.liquidityPool.getReserves();
+        const reserveRatio = BigNumber.from(reserves0).div(BigNumber.from(reserves1));
+
+        return reserveRatio.toString();
+    }
+
+    async getEncodedParams() {
+        const tokenA = await this.getTokenA();
+        let tokenAAmount: string;
+        let minTokenAOut: string;
+
+        const tokenB = await this.getTokenB();
+        let tokenBAmount: string;
+        let minTokenBOut: string; 
+
+        const reserveRatio = await this.getReserveRatio();
+
+        if (tokenA == ohm) {
+            tokenAAmount = this.ohmToBorrow;
+            minTokenAOut = BigNumber.from(tokenAAmount).mul(this.acceptableSlippage).toString();
+
+            tokenBAmount = BigNumber.from(tokenAAmount).div(reserveRatio).toString();
+            minTokenBOut = BigNumber.from(tokenBAmount).mul(this.acceptableSlippage).toString();
+        } else {
+            tokenBAmount = this.ohmToBorrow;
+            minTokenBOut = BigNumber.from(tokenBAmount).mul(this.acceptableSlippage).toString();
+
+            tokenAAmount = BigNumber.from(tokenBAmount).mul(reserveRatio).toString();
+            minTokenAOut = BigNumber.from(tokenAAmount).mul(this.acceptableSlippage).toString();
+        }
+
+        const encodedParams = abiCoder.encode(
+            [ "address", "address", "uint256", "uint256", "uint256", "uint256" ],
+            [ tokenA, tokenB, tokenAAmount, tokenBAmount, minTokenAOut, minTokenBOut ]
+        );
+        return encodedParams;
+    }
+}
